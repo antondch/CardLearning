dbb004bca1a55bb816101f36d65ae8c4b8fcbb15
Crazy Horse <antondch@gmail.com>
2015-03-30 01:24:04 +0300

    fix animation easing & delay

--- LingvoCards/LCCardViewController.m
+++ LingvoCards/LCCardViewController.m
@@ -10,6 +10,7 @@
 
 @interface LCCardViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *enLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *cardBackImage;
 
 @end
 
@@ -24,16 +25,17 @@
     CATransform3D perspectiveTransform = CATransform3DIdentity;
     perspectiveTransform.m34 = 1.0 / -400;
     
-    perspectiveTransform = CATransform3DRotate(perspectiveTransform,  M_PI / 1, 0.0f, 1.0f, 0.0f);
+    perspectiveTransform = CATransform3DRotate(perspectiveTransform,  M_PI / 2, 0.0f, 1.0f, 0.0f);
     //     cardController.view.layer.anchorPoint = CGPointMake(0.0, 0.0);
     self.view.layer.zPosition=500;
     self.view.layer.allowsEdgeAntialiasing = YES;
-    [UIView animateWithDuration:1.4 animations:^{
+    [UIView animateWithDuration:0.5 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
         self.view.layer.transform = perspectiveTransform;
-    }completion:^(BOOL finished) {
-        
-        CATransform3D originalPerspectiveTransform = CATransform3DIdentity;
-        [UIView animateWithDuration:0.9 animations:^{
+    } completion:^(BOOL finished) {
+        self.cardBackImage.hidden = NO;
+        CATransform3D originalPerspectiveTransform = CATransform3DRotate(perspectiveTransform,  M_PI / 2, 0.0f, 1.0f, 0.0f);
+        //CATransform3DIdentity;
+        [UIView animateWithDuration:0.5  delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
             self.view.layer.transform = originalPerspectiveTransform;
         }completion:^(BOOL finished) {
             self.view.layer.zPosition=0;
