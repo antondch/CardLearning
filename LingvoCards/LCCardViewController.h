c8c4ac9fd01b329fb0221f73ab2c3dc025cd553e
Crazy Horse <antondch@gmail.com>
2015-03-29 06:09:51 +0300

    add card xib

---
+++ LingvoCards/LCCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  LCCardViewController.h
+//  LingvoCards
+//
+//  Created by jessie on 29.03.15.
+//  Copyright (c) 2015 DCH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCCardViewController : UIViewController
+
+@end
