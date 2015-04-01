43ce645d60f15bc11770e230eef884901f9f2c04
Crazy Horse <antondch@gmail.com>
2015-04-01 08:10:18 +0300

    text view now is resaziable

---
+++ LingvoCards/ResizeableTExtView.m
@@ -0,0 +1,42 @@
+//
+//  ResizeableTExtView.m
+//  LingvoCards
+//
+//  Created by jessie on 01.04.15.
+//  Copyright (c) 2015 DCH. All rights reserved.
+//
+
+#import "ResizeableTExtView.h"
+
+@implementation ResizeableTExtView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (void) layoutSubviews
+{
+    [super layoutSubviews];
+    
+    if (!CGSizeEqualToSize(self.bounds.size, [self intrinsicContentSize])) {
+        [self invalidateIntrinsicContentSize];
+    }
+}
+
+- (CGSize)intrinsicContentSize
+{
+    CGSize intrinsicContentSize = self.contentSize;
+    
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0f) {
+        intrinsicContentSize.width += (self.textContainerInset.left + self.textContainerInset.right ) / 2.0f;
+        intrinsicContentSize.height += (self.textContainerInset.top + self.textContainerInset.bottom) / 2.0f;
+    }
+    
+    return intrinsicContentSize;
+}
+
+
+@end
