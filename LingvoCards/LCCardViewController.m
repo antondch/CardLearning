0aac0433604c088bcb66e682fb72934f42dbdf09
Crazy Horse <antondch@gmail.com>
2015-04-01 06:30:53 +0300

    rotate and hide all controls

--- LingvoCards/LCCardViewController.m
+++ LingvoCards/LCCardViewController.m
@@ -10,17 +10,19 @@
 #import "LCItemStore.h"
 
 @interface LCCardViewController (){
-    BOOL isRotated;
+
     CGPoint cardOutDistance;
     int order;
 }
+@property (weak, nonatomic) IBOutlet UIButton *soundBtn;
+@property (weak, nonatomic) IBOutlet UIButton *learnBtn;
 @property (weak, nonatomic) IBOutlet UILabel *enLabel;
 @property (weak, nonatomic) IBOutlet UILabel *transcriptionLabel;
 
 @property (weak, nonatomic) IBOutlet UIImageView *cardBackImage;
 @property (weak, nonatomic) IBOutlet UITextField *ruText;
-
-//@property BOOL isRotated;
+@property (strong, nonatomic) UISwipeGestureRecognizer *swipeRecognizer;
+@property (nonatomic) BOOL isRotated;
 
 @end
 
@@ -36,6 +38,8 @@
     self.view.userInteractionEnabled = isActive;
     _isActive = isActive;
     self.enLabel.hidden = !isActive;
+    self.soundBtn.hidden = !isActive;
+    self.learnBtn.hidden = !isActive;
 }
 
 - (void)viewDidLoad {
@@ -47,7 +51,7 @@
     NSLog(@"text: %@",self.enLabel.text);
         NSLog(@"en: %@",self.item.en);
         NSLog(@"transcription: %@",self.item.transcription);
-    isRotated = NO;
+    self.isRotated = NO;
     
     CGSize appSize = [[UIScreen mainScreen] bounds].size;
     CGSize cardSize = self.view.bounds.size;
@@ -57,18 +61,27 @@
     
     UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapRecognize:)];
     [self.view addGestureRecognizer:tapRecognizer];
-    UISwipeGestureRecognizer *swipeRecognizer = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeRecognize:)];
-    swipeRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
-    [self.view addGestureRecognizer:swipeRecognizer];
+    self.swipeRecognizer = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeRecognize:)];
+    self.swipeRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
+    [self.view addGestureRecognizer:self.swipeRecognizer];
 }
 
 
-#pragma mark - animation
+#pragma mark - rotation
+
+- (void)setIsRotated:(BOOL)isRotated{
+    _isRotated = isRotated;
+    if(isRotated){
+        self.swipeRecognizer.direction = UISwipeGestureRecognizerDirectionRight;
+    }else{
+        self.swipeRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
+    }
+}
 
 - (void)rotateView{
     self.item.wasKeeked = YES;
     int direction = 1;
-    if(isRotated){
+    if(self.isRotated){
         direction = -1;
     }
     CATransform3D perspectiveTransform = CATransform3DIdentity;
@@ -80,25 +93,33 @@
     [UIView animateWithDuration:0.5 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
         self.view.layer.transform = perspectiveTransform;
     } completion:^(BOOL finished) {
-        self.cardBackImage.hidden = isRotated;
+        self.cardBackImage.hidden = self.isRotated;
         CATransform3D itemsRevertTransform = CATransform3DIdentity;
         itemsRevertTransform = CATransform3DRotate(self.enLabel.layer.transform, direction*M_PI , 0.0f, 1.0f, 0.0f);
         self.enLabel.layer.transform = itemsRevertTransform;
+        self.learnBtn.layer.transform = itemsRevertTransform;
+        self.transcriptionLabel.hidden = !self.isRotated;
+        self.soundBtn.hidden = !self.isRotated;
         CATransform3D originalPerspectiveTransform = CATransform3DRotate(perspectiveTransform,  M_PI / 2, 0.0f, 1.0f, 0.0f);
         //CATransform3DIdentity;
         [UIView animateWithDuration:0.5  delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
             self.view.layer.transform = originalPerspectiveTransform;
         }completion:^(BOOL finished) {
             self.view.layer.zPosition=0;
-            isRotated = !isRotated;
+            self.isRotated = !self.isRotated;
         }];
     }];
 }
 
 -(void)flyOutView{
+    int direction = 1;
+    if(self.isRotated){
+        direction = -1;
+    }
     self.view.layer.zPosition=10;
-    CATransform3D rotationTransform = CATransform3DMakeTranslation(cardOutDistance.x, cardOutDistance.y,0);
-    rotationTransform = CATransform3DRotate(rotationTransform,-M_PI/2, 0.0f, 0.0f, 0.1);
+    CATransform3D rotationTransform = CATransform3DIdentity;
+    rotationTransform = CATransform3DTranslate(self.view.layer.transform, direction*cardOutDistance.x, cardOutDistance.y,0);
+    rotationTransform = CATransform3DRotate(rotationTransform,direction*-M_PI/2, 0.0f, 0.0f, 0.1);
     if ([_delegate respondsToSelector:@selector(cardWillRemoved:)]){
         [_delegate performSelector:@selector(cardWillRemoved:)withObject:self];
     }
